7ae4c30b9e81f73fae8ddf86715d28b004a346f7
rohith <rohith3085@gmail.com>
2021-09-27 11:30:33 +0000

    Fibionacci Series

--- fibio.c
+++ fibio.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main(){
-    int i,n,a=0,b=1,c;
+    int i,n,a=0,b=1,c,k=1;
     printf("Enter a number : ");
     scanf("%d",&n);
-    printf("%d\n%d\n",a,b);
+    printf("NO.%d\t%d\n",k,a);    
+    printf("NO.%d\t%d\n",k+1,b);    
+
     for(i=3;i<=n;i++){
         c = a + b;
         a = b;
         b = c;
-        printf("%d\n",c);
+        printf("NO.%d\t%d\n",i,c);
     }
     return 0;
 }
