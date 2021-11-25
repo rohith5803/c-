db77653b166f6a5270a851498b6434cc1133be98
rohith <rohith3085@gmail.com>
2021-11-25 18:00:04 +0000

    Datatypes in c

---
+++ datatypes.c
@@ -0,0 +1,14 @@
+// data type in c 
+
+#include<stdio.h>
+int main(){
+    int x =10;
+    float X =2.5;
+    char c ='c';
+    char name[] = "knucklehead";
+    printf("%d\n",x);
+    printf("%f\n",X);
+    printf("%c\n",c);
+    printf("%s\n",name);
+    return 0;
+}
