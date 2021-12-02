6e99daae10f799cef39da7fe48d170e3aa924000
rohith <rohith3085@gmail.com>
2021-12-02 08:05:52 +0000

    Arithmetic operators in c

---
+++ operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter any two numbers : ");
+    scanf("%d %d",&a,&b);
+
+    printf("The sum of these number is: %d\n",a+b);
+    printf("The difference of these number is: %d\n",a-b);
+    printf("The multiplication of these number is: %d\n",a*b);
+    printf("The division quotient of these number is: %d\n",a/b);
+    printf("The division reminder of these number is: %d\n",a%b);
+    return 0;
+
+}
