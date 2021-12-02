9f6cd8f2c098ac1b2e5b5959872a5d2bbcc44d81
rohith <rohith3085@gmail.com>
2021-12-02 08:00:17 +0000

    the basic stucture of c programming

---
+++ structure.c
@@ -0,0 +1,8 @@
+#include<stdio.h> // header file 
+int main(){      // this is were the program starts {starting point}
+
+//the code will be written here 
+
+return 0;       // return 0 in the main function means that the program executed successfully
+}
+ 
